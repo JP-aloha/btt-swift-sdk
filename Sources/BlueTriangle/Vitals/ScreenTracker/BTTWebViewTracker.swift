f240911853fc726aeb114077be11d6c295b36a5a
JP-aloha <jaib@alohatechnology.com>
2024-07-19 14:42:23 +0530

    Added log to BTTWebViewTracker

--- Sources/BlueTriangle/Vitals/ScreenTracker/BTTWebViewTracker.swift
+++ Sources/BlueTriangle/Vitals/ScreenTracker/BTTWebViewTracker.swift
@@ -11,6 +11,7 @@ import WebKit
 public class BTTWebViewTracker {
      
     static var shouldCaptureRequests = false
+    static var logger : Logging?
     
     public static func webView( _ webView: WKWebView, didCommit navigation: WKNavigation!){
        
@@ -29,8 +30,14 @@ public class BTTWebViewTracker {
         let sessionJavascript = String(format: "localStorage.setItem(\"%@\", JSON.stringify(%@))", "BTT_X0siD", BTTSessionValues)
         
         webView.evaluateJavaScript(sessionJavascript as String) { (result, error) in
-            if error == nil {}
-            else{}
+            
+            if let error =  error {
+                BTTWebViewTracker.logger?.error("BlueTriangle: WebViewTracker: Error while injecting session \(sessionId) : \(error)")
+            }
+            else{
+                BTTWebViewTracker.logger?.info("BlueTriangle: WebViewTracker: Successfully injected session \(sessionId)")
+            }
+
         }
     }
     
@@ -38,13 +45,18 @@ public class BTTWebViewTracker {
         
         //Version
         let sdkVersion = "iOS-\(Version.number)"
+        let sessionId = "\(BlueTriangle.sessionID)"
         let expiration = NSString(string:"\(Date.addCurrentTimeInMinut(18000))")
         let BTTVersionValues = String(format: "{\"value\":\"%@\", \"expires\":\"%@\"}", sdkVersion, expiration)
         let versionJavascript = String(format: "localStorage.setItem(\"%@\", JSON.stringify(%@))", "BTT_SDK_VER", BTTVersionValues)
         
         webView.evaluateJavaScript(versionJavascript as String) { (result, error) in
-            if error == nil {}
-            else{}
+            if let error =  error {
+                BTTWebViewTracker.logger?.error("BlueTriangle: WebViewTracker: Error while injecting version for the session  \(sessionId) : \(error)")
+            }
+            else{
+                BTTWebViewTracker.logger?.info("BlueTriangle: WebViewTracker: Successfully injected version for the session \(sessionId)")
+            }
         }
     }
     
@@ -54,13 +66,18 @@ public class BTTWebViewTracker {
         if BTTWebViewTracker.shouldCaptureRequests {
             //WCD
             let isEnableTracking = "on"
+            let sessionId = "\(BlueTriangle.sessionID)"
             let expiration = NSString(string:"\(Date.addCurrentTimeInMinut(18000))")
             let BTTWCDValues = String(format: "{\"value\":\"%@\", \"expires\":\"%@\"}", isEnableTracking, expiration)
             let wcdJavascript = String(format: "localStorage.setItem(\"%@\", JSON.stringify(%@))", "BTT_WCD_Collect", BTTWCDValues)
             
             webView.evaluateJavaScript(wcdJavascript as String) { (result, error) in
-                if error == nil {}
-                else{}
+                if let error =  error {
+                    BTTWebViewTracker.logger?.error("BlueTriangle: WebViewTracker: Error while injecting WCDValues for the session \(sessionId) : \(error)")
+                }
+                else{
+                    BTTWebViewTracker.logger?.info("BlueTriangle: WebViewTracker: Successfully injected WCDValues for the session \(sessionId)")
+                }
             }
         }
     }
